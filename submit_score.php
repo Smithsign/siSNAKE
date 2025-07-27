8f2d7d92df17d7439c508ef69a7e154428ef26f1
Smithsign <ivanwageemail@gmail.com>
2025-07-27 11:42:37 +0800

    Update submit_score.php

--- submit_score.php
+++ submit_score.php
@@ -1,16 +1,5 @@
 <?php
-$host = "localhost";
-$user = "your_user";
-$pass = "your_pass";
-$dbname = "snake_game";
-$conn = new mysqli($host, $user, $pass, $dbname);
-
+include 'db.php';
 $data = json_decode(file_get_contents("php://input"), true);
-$name = $conn->real_escape_string($data["username"]);
-$image = $conn->real_escape_string($data["image"]);
-$score = (int)$data["score"];
-
-$sql = "INSERT INTO leaderboard (username, image, score) VALUES ('$name', '$image', '$score')";
-$conn->query($sql);
-$conn->close();
-?>
+$stmt = $pdo->prepare("INSERT INTO leaderboard (username, image, score) VALUES (:u, :i, :s)");
+$stmt->execute(['u' => $data['username'], 'i' => $data['image'], 's' => $data['score']]);
