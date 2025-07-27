384c4bb0e2ee174fa2615bf8711d88b023e80c69
Smithsign <ivanwageemail@gmail.com>
2025-07-27 11:11:30 +0800

    Update game.js

--- game.js
+++ game.js
@@ -2,8 +2,11 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const box = 20;
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
+let canvasWidth = window.innerWidth < 600 ? 300 : 600;
+let canvasHeight = window.innerHeight < 600 ? 300 : 600;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 
 let snake;
 let direction;
@@ -13,9 +16,17 @@ let game;
 let isGameRunning = false;
 
 const foodImg = new Image();
-foodImg.src = "orange.png"; // make sure orange.png is in the same folder
+foodImg.src = "orange.png";
+
+const headImg = new Image();
+headImg.src = "xin.jpg";
 
 function initGame() {
+  canvasWidth = window.innerWidth < 600 ? 300 : 600;
+  canvasHeight = window.innerHeight < 600 ? 300 : 600;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
   snake = [{ x: 5 * box, y: 5 * box }];
   direction = "RIGHT";
   score = 0;
@@ -38,10 +49,14 @@ function draw() {
 
   // Draw snake
   for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i === 0 ? "#00FF7F" : "#FFA500";
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
-    ctx.strokeStyle = "#111";
-    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    if (i === 0) {
+      ctx.drawImage(headImg, snake[i].x, snake[i].y, box, box);
+    } else {
+      ctx.fillStyle = "#FFA500";
+      ctx.fillRect(snake[i].x, snake[i].y, box, box);
+      ctx.strokeStyle = "#111";
+      ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
   }
 
   // Draw food
@@ -110,3 +125,11 @@ document.addEventListener("keydown", (e) => {
 // Start & Restart buttons
 document.getElementById("startBtn").addEventListener("click", initGame);
 document.getElementById("tryAgainBtn").addEventListener("click", initGame);
+
+// Resize canvas on window resize
+window.addEventListener("resize", () => {
+  if (isGameRunning) {
+    clearInterval(game);
+    initGame(); // restart game to re-calculate size
+  }
+});
